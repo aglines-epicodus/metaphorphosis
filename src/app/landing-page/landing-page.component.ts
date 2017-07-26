e5b9ef657b84f1a057c2e0081036f7e2738b7b00
Brian Rush and Jared Eiseman <brian+jared@>
2017-07-26 15:28:59 -0700

    change out landing page design for actual landing page content, set routes for metaphors

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,19 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { DatamuseService} from '../services/datamuse.service';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
+  animations: [
+    trigger('left', [
+      transition(':enter', animate('2000ms ease-in-out', keyframes([
+        style({left: '-25px', opacity:0, offset: 0}),
+        style({left: '50px', opacity:.5, offset: .5}),
+        style({left: '0', opacity:1, offset: 1}),
+      ])))
+    ]),
+    trigger('right', [
+      transition(':enter', animate('2000ms ease-in-out', keyframes([
+        style({right: '-25px', opacity:0, offset: 0}),
+        style({right: '50px', opacity:.5, offset: .5}),
+        style({right: '0', opacity:1, offset: 1}),
+      ])))
+    ]),
+    trigger('top', [
+      transition(':enter', animate('2000ms ease-in-out', keyframes([
+        style({top: '-25px', opacity:0, offset: 0}),
+        style({top: '50px', opacity:.5, offset: .5}),
+        style({top: '0', opacity:1, offset: 1}),
+      ])))
+    ]),
+    trigger('bottom', [
+      transition(':enter', animate('2000ms ease-in-out', keyframes([
+        style({bottom: '-25px', opacity:0, offset: 0}),
+        style({bottom: '50px', opacity:.5, offset: .5}),
+        style({bottom: '0', opacity:1, offset: 1}),
+      ])))
+    ]),
+  ]
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit, AfterViewChecked {
+  state: string = 'start';
 
   constructor() { }
 
+  ngAfterViewChecked() {
+    // this.state = 'finish';
+  }
 
   ngOnInit() {
 
 
   }
 
+  triggerAnimation() {
+    this.state = 'finish';
+  }
+
 }
+
+
+/*
+animations: [
+  trigger('slide-mobile', [
+    state('start', style({
+      right: '-200px',
+    })),
+    state('finish', style({
+      right: '0px',
+    })),
+    transition('start <=> finish', animate('500ms ease-in-out'))
+  ])
+]
+*/
