dd18a807509b076a87ddc5413f513d727e0120d6
Brian Rush and Jared Eiseman <brian+jared@>
2017-07-27 15:07:19 -0700

    add screenshots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,47 +29,7 @@ export class AppComponent implements OnInit {
     }
   }
 
-  // toggleMobile() {
-  //   console.log(this.mobileShown);
-  //   if (this.mobileShown === 'true') {
-  //     this.mobileShown = 'false';
-  //   } else if (this.mobileShown === 'false' || this.mobileShown === 'neither'){
-  //     this.mobileShown = 'true';
-  //   } else {
-  //     this.mobileShown = 'neither';
-  //   }
-  // }
-  //
-  // checkMobileVisibility() {
-  //   if (this.mobileShown === 'true') {
-  //     return 'slide-mobile-in';
-  //   } else if (this.mobileShown === 'false'){
-  //     return 'slide-mobile-out'
-  //   } else {
-  //     return '';
-  //   }
-  //
-  // }
-
   ngOnInit() {
 
-    //TODO: Rewrite animation here to use angular animation modules
-    //TODO: Tutorial here: https://coursetro.com/posts/code/63/Angular-4-Animation-Tutorial
-    // var el = document.getElementById('text');
-    // var newHtml = el.innerHTML;
-    // var newerHtml = newHtml.split("");
-    // var newestHtml = newerHtml.map(function(letter) {
-    //   return '<span class="letter">' + letter + '</span>';
-    // }).join("");
-    // el.innerHTML = newestHtml;
-    //
-    // var step = 0;
-    // var els = document.getElementsByClassName('letter');
-    // var directions = ['top', 'bottom', 'left', 'right'];
-    // for (var i = 0; i < els.length; i++) {
-    //   (els[i] as HTMLElement).style.animationDelay = step + 's';
-    //   step = Math.random() * .4;
-    //   els[i].classList.add('animate-' + directions[Math.floor(Math.random() * 4)]);
-    // }
   }
 }
